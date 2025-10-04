965b610b75a4b9c6155570ec6440d67a63ef40e8
Mariia <mariadenkovic4@gmail.com>
2025-10-05 02:35:53 +0300

    add new-hw12

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import {
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-const form = document.querySelector(".search-form");
+const form = document.querySelector(".form");
 const input = document.querySelector("input[name='query']");
 const loadMoreBtn = document.querySelector(".load-more");
 
